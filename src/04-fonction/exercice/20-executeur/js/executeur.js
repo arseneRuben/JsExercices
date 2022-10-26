27e296a2accb329ed7c24d04dd3ae334453818d8
isi <isi@751FJW2>
2022-10-26 11:57:21 -0400

    20-executeur

--- src/04-fonction/exercice/20-executeur/js/executeur.js
+++ src/04-fonction/exercice/20-executeur/js/executeur.js
@@ -1,6 +1,6 @@
 'use strict'
 
-function execute (op, a, b) {
+function execute (operateur, a, b) {
     function add (a, b) {
         return a + b
     }
@@ -11,25 +11,30 @@ function execute (op, a, b) {
     function mul (a, b) {
         return a * b
     }
-
-    switch (typeof op) {
-    case 'string':
-        switch (op) {
+    let result
+    if (typeof (operateur) === 'string') {
+        switch (operateur) {
         case 'ADD':
-            return add(a, b)
+            result = add(a, b)
+            break
         case 'SUB':
-            return sub(a, b)
+            result = sub(a, b)
+            break
         case 'MUL':
-            return mul(a, b)
+            result = mul(a, b)
+            break
         default:
-            return "Nom de l'operation inconnu"
+            result = "Nom de l'operation inconnu"
+            break
         }
-
-    case 'number':
-        return "Type de l'operation inconnu"
-    case 'function':
-        return op(a, b)
-    default:
-        return "Nom de l'operation inconnu"
     }
+    if (typeof (operateur) === 'function') {
+        result = operateur(a, b)
+    }
+
+    if (typeof (operateur) === 'number') {
+        result = "Type de l'operation inconnu"
+    }
+
+    return result
 }
