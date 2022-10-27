ed841b0b48df712356a8526b104902a443f355ff
isi <isi@751FJW2>
2022-10-27 11:04:41 -0400

    40-random

--- src/05-objet-natif/exercice/50-total/js/random.js
+++ src/05-objet-natif/exercice/50-total/js/random.js
@@ -11,10 +11,8 @@ const initial = [1, 2, 3, 4, 5, 6]
 const resultat = []
 
 // Compléter le code pour remplir le Array resultat dans un ordre aléatoire
-// Premier trie aleatoire du tableau initial
 const temp = initial.sort(() => Math.random() - 0.5)
 temp.forEach((element, index) => {
-    // Construction des element du tableau resultant a partir du tableau trier aleatoirement
     resultat[index] = element
 })
 console.log('Résultat', resultat)
