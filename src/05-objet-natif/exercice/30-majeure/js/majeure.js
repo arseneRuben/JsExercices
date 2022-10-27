4a4a758ceb71d09f723f24fcc0c61d6ee42cbbe6
isi <isi@751FJW2>
2022-10-27 10:12:46 -0400

    30-majeure

---
+++ src/05-objet-natif/exercice/30-majeure/js/majeure.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// Age de la majorité
+const MAJOR = 18
+
+// Les âges de 4 groupes de personne
+const AGES = [
+    [25, 39, 51, 22, 15, 44],
+    [25, 39, 51, 22, 55, 44],
+    [25],
+    [15]
+]
+
+AGES.forEach((groupe, index) => {
+    let result
+    let i = 0
+    let item = groupe[i]
+    while (item >= MAJOR && i <= groupe.length) {
+        i++
+        item = groupe[i]
+    }
+    if (i === groupe.length) { result = 'sont tous adultes' } else { result = 'ne sont pas tous adultes' }
+
+    document.write('<tr><td> groupe ' + index + '</td><td>' + result + '</td></tr>')
+})
