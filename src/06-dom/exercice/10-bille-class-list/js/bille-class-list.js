a23e46621bd415da1dd2cc3ad3ed4ce53c42fa5f
isi <isi@751FJW2>
2022-10-31 10:25:28 -0400

    10-bille-class-list

--- src/06-dom/exercice/10-bille-class-list/js/bille-class-list.js
+++ src/06-dom/exercice/10-bille-class-list/js/bille-class-list.js
@@ -1,25 +1,25 @@
 'use strict'
 const firstCanvas = document.querySelector('div.bille:nth-child(1)')
-firstCanvas.classList += ' arrondi'
-firstCanvas.classList += ' decale'
+firstCanvas.classList.add('arrondi')
+firstCanvas.classList.add('decale')
 
 const secondCanvas = document.querySelector('div.bille:nth-child(2)')
-secondCanvas.classList += ' taille-x'
-secondCanvas.classList += ' orange'
-secondCanvas.classList += ' decale'
+secondCanvas.classList.add('taille-x')
+secondCanvas.classList.add('orange')
+secondCanvas.classList.add('decale')
 
 const thirdCanvas = document.querySelector('div.bille:nth-child(3)')
-thirdCanvas.classList += ' incline'
-thirdCanvas.classList += ' taille-xx'
-thirdCanvas.classList += ' decale'
+thirdCanvas.classList.add('incline')
+thirdCanvas.classList.add('taille-xx')
+thirdCanvas.classList.add('decale')
 
 const fourthCanvas = document.querySelector('div.bille:nth-child(4)')
-fourthCanvas.classList += ' mauve'
-fourthCanvas.classList += ' taille-xx'
-fourthCanvas.classList += ' decale'
-fourthCanvas.classList += ' arrondi'
+fourthCanvas.classList.add('mauve')
+fourthCanvas.classList.add('taille-xx')
+fourthCanvas.classList.add('decale')
+fourthCanvas.classList.add('arrondi')
 
 const fifthCanvas = document.querySelector('div.bille:nth-child(5)')
-fifthCanvas.classList += ' mauve'
-fifthCanvas.classList += ' decale'
-fifthCanvas.classList += ' arrondi'
+fifthCanvas.classList.add('mauve')
+fifthCanvas.classList.add('decale')
+fifthCanvas.classList.add('arrondi')
