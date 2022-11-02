194dd31655a4536a7c1ec4c5404a237fc17debaf
isi <isi@751FJW2>
2022-11-02 12:20:53 -0400

    30-jour-semaine

--- src/07-evenment/exercice/30-jour-semaine/js/jour-semaine.js
+++ src/07-evenment/exercice/30-jour-semaine/js/jour-semaine.js
@@ -3,10 +3,24 @@
 const JOURS_FR = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
 const JOURS_EN = ['Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Saturday', 'Sunday']
 
-const checkBoxElement = document.getElementById('language')
-const rangeElement = document.getElementById('dayNumber')
-console.log(checkBoxElement)
-/*
-checkBoxElement.addEventListener('change', function (event) {
-    console.log(checkBoxElement.value)
-}) */
+window.addEventListener('DOMContentLoaded', function loaded (event) {
+    window.removeEventListener('DOMContentLoaded', loaded, false)
+
+    const checkBoxElement = document.getElementById('language')
+
+    function changeLanguage (event, jours) {
+        if (checkBoxElement.checked) {
+            jours = JOURS_EN
+        } else {
+            jours = JOURS_FR
+        }
+    }
+
+    const rangeElement = document.getElementById('dayNumber')
+
+    let jours
+    checkBoxElement.addEventListener('change', function (event) {
+        changeLanguage(event, jours)
+    })
+    console.log(jours)
+}, false)
