498ac5b1673c9629f6ff289fa71635fa697721c7
isi <isi@751FJW2>
2022-11-03 12:50:51 -0400

    70-cascade-select

---
+++ src/07-evenment/exercice/70-cascade-select/js/cascade-select.js
@@ -0,0 +1,31 @@
+const countrySelect = document.getElementById('country_id')
+const citySelect = document.getElementById('city_id')
+
+Object.keys(COUNTRIES).forEach(country => {
+    const optionElement = document.createElement('option')
+    optionElement.value = country
+    const textNode = document.createTextNode(country)
+    optionElement.appendChild(textNode)
+    countrySelect.appendChild(optionElement)
+})
+
+countrySelect.addEventListener('input', updateCitySelect)
+
+function updateCitySelect (event) {
+    const country = event.target.value
+    citySelect.innerHTML = null
+    COUNTRIES[country].forEach(city => {
+        const optionElement = document.createElement('option')
+        optionElement.value = city
+        const textNode = document.createTextNode(city)
+        optionElement.appendChild(textNode)
+        citySelect.appendChild(optionElement)
+    })
+}
+
+citySelect.addEventListener('input', updateOutput)
+
+function updateOutput (event) {
+    const output = document.getElementById('output')
+    output.innerHTML = event.target.value
+}
