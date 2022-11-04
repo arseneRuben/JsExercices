17207795e162a3a02c327f86a849f795cf8be0af
isi <isi@751FJW2>
2022-11-04 09:35:03 -0400

    80-spirit

--- src/07-evenment/exercice/80-sprite/js/sprite.js
+++ src/07-evenment/exercice/80-sprite/js/sprite.js
@@ -3,16 +3,15 @@
 const IMAGE_WIDTH = 587
 // Largeur de tout le fichier spritesheet.png
 const SPRITE_WIDTH = 35220
-
+let i = 1
 window.addEventListener('DOMContentLoaded', function loaded (event) {
     window.removeEventListener('DOMContentLoaded', loaded, false)
     const knight = document.getElementById('knight')
 
-    const intervalID = setInterval(setBackgroundImage, 500)
-    let i = 1
+    const intervalID = setInterval(setBackgroundImage, 250)
     function setBackgroundImage () {
-        // console.log("url(decodeURI('../freeknight/png/Jump (" + (i++) + ").png'))")
-        knight.style.backgroundImage = "url(decodeURI('../freeknight/png/Jump (" + (i++) + ").png'))"
+        knight.style.backgroundImage = 'url(js/spritesheet.png)'
+        knight.style.backgroundPosition = 'top 0px left ' + IMAGE_WIDTH * i++ + 'px'
 
         if (i === 10) i = 1
     }
