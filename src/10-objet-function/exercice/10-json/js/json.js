ba19ecd59ee9bb7f0f98c295894ff3f0802ed457
isi <isi@751FJW2>
2022-11-08 05:16:01 -0500

    10-json

--- src/10-objet-function/exercice/10-json/js/json.js
+++ src/10-objet-function/exercice/10-json/js/json.js
@@ -11,13 +11,19 @@ window.addEventListener('DOMContentLoaded', function loaded (event) {
     const form = document.getElementsByTagName('form')[0]
     form.addEventListener('submit', treatment)
 
+    /**
+     * processing of values submitted via the form
+     * @param {*} event
+     */
     function treatment (event) {
         event.preventDefault()
+        // form values
         const user = {
             firstname: firstName.value,
             lastname: lastName.value,
             age: old.value
         }
+        // Show JSON object in pre tag
         output.innerHTML = JSON.stringify(user, null, '  ')
     }
 }, false)
