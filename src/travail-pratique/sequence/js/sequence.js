7aebd5b033e2d6c53258e1749192f7048ce3e86d
isi <isi@751FJW2>
2022-11-17 10:02:29 -0500

    final exan

--- src/travail-pratique/sequence/js/sequence.js
+++ src/travail-pratique/sequence/js/sequence.js
@@ -7,7 +7,6 @@ const Sequence = (function () {
     let number5To8Id
     let messageBlock
     let difficulty
-    const maxStep = 4
     let times = 0
     let sequencePresentationMessage
     let yourTurnMessage
@@ -15,8 +14,8 @@ const Sequence = (function () {
     const progressIncrement = 6
 
     const SIZE = 4
-    const SHOW_TIME = 2000
-    const USER_TIME = 2000
+    const STEP_SHOW_TIME = 2000
+    const USER_TIME = 1000
     // Round's levels
     const levels = {
         NORMAL: 'NORMAL',
@@ -25,11 +24,20 @@ const Sequence = (function () {
 
     // Different possible states of the steps
     const stepStates = {
+        NEW: 'NEW',
         CHOOSEN: 'CHOOSEN',
         WAITING: 'WAITING',
         COMPLETED: 'COMPLETED'
     }
 
+    // Sets the maximum number of stes depending on the difficulty level of the round
+    function maxSize (round) {
+        let output = SIZE
+        if (round.level === levels.DIFFICULT) {
+            output = SIZE * 2
+        }
+        return output
+    }
     /**
      * Returns the correct list of number items according to the level, either the first 4 items (normal level) or all 8 (difficult level)
      */
@@ -41,16 +49,36 @@ const Sequence = (function () {
         return output
     }
 
+    /**
+     * new round
+     */
+
+    function newRound () {
+        let round
+        const sequence = new Sequence([])
+        if (difficulty.value === '0') {
+            round = new Round(levels.NORMAL, sequence)
+        } else {
+            round = new Round(levels.DIFFICULT, sequence)
+        }
+        return round
+    }
+    /**
+     * adds the second list of numbers in case of difficult level
+     */
     function setNumberIdHandler () {
-        console.log('display')
         if (difficulty.value === '1') {
             number5To8Id.style.display = 'block'
         }
     }
 
+    /**
+     * changes the progress bar
+     */
     function runProgress () {
         clearInterval(progressIntervalId)
         progressIntervalId = setInterval(evolve, 1)
+
         function evolve () {
             if (progress.value === progress.max) {
                 clearInterval(progressIntervalId)
@@ -85,12 +113,7 @@ const Sequence = (function () {
             difficulty.addEventListener('change', setNumberIdHandler)
 
             btnStart.addEventListener('click', function (event) {
-                const sequence = new Sequence([])
-                if (difficulty.value === '0') {
-                    round = new Round(levels.NORMAL, sequence)
-                } else {
-                    round = new Round(levels.DIFFICULT, sequence)
-                }gi
+                round = newRound()
                 round.playSequence()
                 // btnStop takes place of btnStart
                 btnStart.style.display = 'none'
@@ -139,7 +162,7 @@ const Sequence = (function () {
         }
 
         setState (state) {
-            if (state === stepStates.WAITING) {
+            if (state === stepStates.WAITING || state === stepStates.NEW) {
                 this.hide()
             }
 
@@ -155,8 +178,9 @@ const Sequence = (function () {
 
     class Sequence {
         // we assume that the steps a collected in an array
-        constructor (steps) {
+        constructor (steps, pendingStepIndex = 0) {
             this.steps = steps
+            this.pendingStepIndex = pendingStepIndex
         }
 
         addStep (newStep) {
@@ -166,23 +190,35 @@ const Sequence = (function () {
         clearSteps () {
             this.steps.forEach(step => step.setState(stepStates.WAITING))
         }
+
+        nextPendingStepIndex () {
+            if (this.pendingStepIndex < this.steps.length) {
+                this.pendingStepIndex++
+            } else {
+                this.pendingStepIndex = 0
+            }
+        }
+
+        resetSteps () {
+            this.steps.forEach(step => {
+                step.setState(stepStates.NEW)
+            })
+        }
     }
 
     class Round {
         // for automatic selection of new steps
         #currentLabelList = []
-        #max
+        #maxSize
+        #maxStep
 
         constructor (level = levels.NORMAL, sequence) {
             this.level = level
             this.sequence = sequence
-
-            if (this.level === levels.NORMAL) {
-                this.#max = SIZE
-            } else {
-                this.#max = SIZE * 2
-            }
-            for (let i = 1; i <= this.#max; i++) {
+            this.#maxStep = 1
+            // either 4 or 8 depending on the level
+            this.#maxSize = maxSize(this)
+            for (let i = 1; i <= this.#maxSize; i++) {
                 this.#currentLabelList.push(i)
             }
         }
@@ -192,23 +228,22 @@ const Sequence = (function () {
             let newPosition
 
             times++
+
             // Stop  and wait
-            if (times > maxStep) {
-                this.intervalId = setTimeout(this.readSequence.bind(this), SHOW_TIME / maxStep)
+            if (times > this.#maxStep) {
+                this.intervalId = setTimeout(this.readSequence.bind(this), USER_TIME)
+                times = 0
                 return 0
             }
-            if (this.sequence.steps.length < this.#max) {
-                // A revoire
-                newPosition = Math.floor(Math.random() * (this.#max - this.sequence.steps.length))
-
+            if (this.sequence.steps.length < this.#maxSize) {
+                newPosition = Math.floor(Math.random() * (this.#maxSize - this.sequence.steps.length))
                 newStep = this.#currentLabelList[newPosition]
-                // console.log(newStep)
                 this.#currentLabelList.splice(newPosition, 1)
-                // console.log(this.#currentLabelList)
                 this.sequence.addStep(new Step(newStep, stepStates.CHOOSEN, this.level))
             }
+
             // recursif call
-            this.intervalId = setTimeout(this.extendSequence.bind(this), SHOW_TIME / maxStep)
+            this.intervalId = setTimeout(this.extendSequence.bind(this), STEP_SHOW_TIME)
         }
 
         playSequence () {
@@ -234,13 +269,36 @@ const Sequence = (function () {
             this.sequence.clearSteps()
         }
 
+        // Check if the step chosen by the player is in its correct position in the sequence
+        checkStep (step) {
+            return this.sequence.steps.indexOf(step) === this.sequence.pendingStepIndex
+        }
+
         readSequence () {
+            const currentRound = this
+
+            // Passage to the waiting state of each of the stages
             this.sequence.steps.forEach(step => {
                 step.setState(stepStates.WAITING)
+
                 step.getDivCell().addEventListener('click', function (event) {
-                    step.setState(stepStates.COMPLETED)
+                    if (currentRound.checkStep(step)) {
+                        console.log(currentRound.sequence.pendingStepIndex, currentRound.#maxStep)
+                        currentRound.sequence.nextPendingStepIndex()
+                        console.log(currentRound.sequence.pendingStepIndex, currentRound.#maxStep)
+                        step.setState(stepStates.COMPLETED)
+                        if (currentRound.sequence.pendingStepIndex === currentRound.#maxStep) {
+                            //  currentRound.sequence.resetSteps()
+
+                            currentRound.sequence.nextPendingStepIndex()
+                            currentRound.extendSequence()
+                        }
+                    } else {
+                        alert('echec')
+                    }
                 })
             })
+
             // The following message is displayed when the sequence presentation ends: Your turn to play the sequence
             messageBlock.innerHTML = yourTurnMessage
         }
@@ -258,7 +316,6 @@ const Sequence = (function () {
         yourTurnMessage = param.status.messages.read
         const menu = new Menu(param.menus)
         menu.display()
-        // setInterval(console.log(menu), 40)
         // new Party
     }
 
